84d0b53f503e7f942c6145229825bd42b9c46c63
Uyen Nguyen <1851050182uyen@ou.edu.vn>
2021-08-01 11:13:46 +0700

    about us

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -1,16 +1,109 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using TracyShop.Models;
 
 namespace TracyShop.Controllers
 {
     public class AboutController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<AboutController> _logger;
+
+        public AboutController(ILogger<AboutController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("/about", Name = "cart")]
+        public IActionResult About()
         {
             return View();
+        }
+
+        // GET: AboutController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AboutController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AboutController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AboutController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AboutController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AboutController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AboutController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AboutController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
