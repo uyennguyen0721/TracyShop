9b5a8222ff57c8bf2b723857fc618fb371c2a253
Uyen Nguyen <1851050182uyen@ou.edu.vn>
2021-10-01 19:56:55 +0700

    reviews for admin

---
+++ Areas/Admin/Controllers/ReviewsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TracyShop.Data;
+
+namespace TracyShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ReviewsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ReviewsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int? id)
+        {
+            if(id == null)
+            {
+                ViewBag.Id = true;
+                if (_context.Reviews.ToList().Count != 0)
+                {
+                    ViewBag.Message = "";
+                    var reviews = _context.Reviews.ToList().OrderByDescending(p => p.CreatedDate).OrderByDescending(p => p.Rate);
+                    return View(reviews);
+                }
+                else
+                {
+                    ViewBag.Message = "Chưa có phản hồi.";
+                    return View();
+                }
+            }
+            else
+            {
+                ViewBag.Id = false;
+                if (_context.Reviews.Where(r => r.ProductId == id).ToList().Count != 0)
+                {
+                    ViewBag.Message = "";
+                    var review = _context.Reviews.Where(r => r.ProductId == id).ToList().OrderByDescending(p => p.CreatedDate).OrderByDescending(p => p.Rate);
+                    return View(review);
+                }
+                else
+                {
+                    ViewBag.Message = "Chưa có phản hồi.";
+                    return View();
+                }
+            }
+        }
+    }
+}
